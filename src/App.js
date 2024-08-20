274fb0cd8bd43c0b9f447ea6ac600e1310102373
Pietroro98 <pietroro98@gmail.com>
2024-08-20 10:00:47 +0200

    add bootstrao and react  bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; //importo bootstrao per farlo funzionare
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='text-success mt-5 border border-danger'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a

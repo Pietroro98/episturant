6e76b0e0b7f138901baeaa23a5a3110737a9bc0e
Pietroro98 <pietroro98@gmail.com>
2024-08-20 10:03:44 +0200

    MODIFICHE

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/*ORA SIAMO LIBEERI DI UTILIZZARE LE CLASSI BOOTSTRAP*/}
         <p className='text-success mt-5 border border-danger'>
           Edit <code>src/App.js</code> and save to reload.
         </p>

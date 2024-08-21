cb5d383e1a22ef7bf14f18db6f5bc7731d384f6a
Pietroro98 <pietroro98@gmail.com>
2024-08-21 09:28:40 +0200

    aggiunta commenti

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,14 @@ import { Container, Row, Col, Carousel, ListGroup } from "react-bootstrap";
 import pastasciutte from "../Data/Menu.json";
 import { Component } from "react";
 
-class Home extends Component {
+class Home extends Component { // stiamo creando un vero e proprio componente react
+  //memoria del componente, active pasta sarebbe quella attiva
+  // pastasciutte[0] perche è la prima slide che viene generata aprendo la pagina
   state = {
     activePasta: pastasciutte[0],
   };
+
+
   render() {
     return (
       <Container>
@@ -18,16 +22,19 @@ class Home extends Component {
         <Row className="justify-content-center">
           <Col xs={12} md={6} lg={4}>
             <Carousel
-              onSlide={(i) => {
+              onSlide={(i) => {  // metodo in cuui aggiorniamo la pasta ogni volta che il carosello va avanti o indietro
                 console.log("slidecambiata", i);
                 // dobbiamo settare il setState() per cambiare il componente
                 // di stato come nuova active slide
-                this.setState({
+                this.setState({  
+                  // [i] è l'indice della slide che sta arrivando, dopo [0] al cambio
+                  // questo viene sovrapposto a quello inziale per farlo cambiare
+                  // sennò non avremmo aggiornato lo stato del cambio
                   activePasta: pastasciutte[i],
                 });
               }}
             >
-              {pastasciutte.map((pasta) => {
+              {pastasciutte.map((pasta) => { //mappiamo  le slide
                 return (
                   <Carousel.Item key={pasta.id}>
                     <img
@@ -48,7 +55,7 @@ class Home extends Component {
         <Row className="justify-content-center">
           <Col>
             <ListGroup className="text-center">
-              {this.state.activePasta.comments.map((c) => {
+              {this.state.activePasta.comments.map((c) => { // legge i commenti della lista e li fa cambiare con il .map()
                 return (
                   <ListGroup.Item key={c.id}>
                     {c.rating} | {c.comment}

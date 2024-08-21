7a01aea5a9c811760deaf42e6853cb328fdc9284
Pietroro98 <pietroro98@gmail.com>
2024-08-21 11:56:17 +0200

    inserisco il onclick per tutti i form e nel check form cambio il value con il checked

--- src/components/TableBooking.jsx
+++ src/components/TableBooking.jsx
@@ -47,6 +47,8 @@ class TableBooking extends Component {
                       },
                     });
                   }}
+                  // colleghiamo l'interfaccia allo stato al valore imput
+                  value={this.state.reservation.name}
                 />
               </Form.Group>
 
@@ -56,12 +58,32 @@ class TableBooking extends Component {
                   type="tel"
                   placeholder="Numero di telefono:"
                   required
+                  onChange={(e) => {
+                    this.setState({
+                      reservation: {
+                        ...this.state.reservation,
+                        phone: e.target.value,
+                      },
+                    });
+                  }}
+                  value={this.state.reservation.phone}
                 />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>numero di persone</Form.Label>
-                <Form.Select aria-label="Default select example">
+                <Form.Select
+                  required
+                  onChange={(e) => {
+                    this.setState({
+                      reservation: {
+                        ...this.state.reservation,
+                        numberOfPeople: e.target.value,
+                      },
+                    });
+                  }}
+                  value={this.state.reservation.numberOfPeople}
+                >
                   <option>1</option>
                   <option>2</option>
                   <option>3</option>
@@ -69,17 +91,53 @@ class TableBooking extends Component {
                   <option>5</option>
                   <option>6</option>
                 </Form.Select>
-                <Form.Check type="checkbox" label="Fumatori?" />
+                <Form.Check
+                  type="checkbox"
+                  label="Tavolo Fumatori?"
+                  onChange={(e) => {
+                    this.setState({
+                      reservation: {
+                        ...this.state.reservation,
+                        smoking: e.target.checked,
+                      },
+                    });
+                  }}
+                  checked={this.state.reservation.smoking}
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Quando vuole venire?</Form.Label>
-                <Form.Control type="datetime-local" />
+                <Form.Control
+                  type="datetime-local"
+                  required
+                  onChange={(e) => {
+                    this.setState({
+                      reservation: {
+                        ...this.state.reservation,
+                        dateTime: e.target.value,
+                      },
+                    });
+                  }}
+                  value={this.state.reservation.dateTime}
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Richieste speciali</Form.Label>
-                <Form.Control as="textarea" rows={5} />
+                <Form.Control
+                  as="textarea"
+                  rows={5}
+                  onChange={(e) => {
+                    this.setState({
+                      reservation: {
+                        ...this.state.reservation,
+                        specialRequests: e.target.value,
+                      },
+                    });
+                  }}
+                  value={this.state.reservation.specialRequests}
+                />
               </Form.Group>
 
               <Button variant="primary" type="submit">
